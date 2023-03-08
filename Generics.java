6d906dfab2ae3157e976615b84acddec7d019ddd
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-03-08 15:38:07 +0530

    added : code for find max string value

--- Generics.java
+++ Generics.java
@@ -6,10 +6,14 @@ public class Generics {
         Generics findMaxIntNumber = new Generics();
         Integer number1 = 10, number2 = 10, number3 = 40;
         Float floatNumber1 = 2.5f, floatNumber2 = 10.3f, floatNumber3 = 7.6f;
+        String name1 = "Apple", name2 = "Banana", name3 = "Mango";
+
         Integer max = findMaxIntNumber.findMaxIntNum(number1, number2, number3);
         System.out.println("Max  Integer number  is : " + max);
         Float maxFloatNumber = new Generics().findMaxNum(floatNumber1,floatNumber2,floatNumber3);
         System.out.println("Max float number is : " + maxFloatNumber);
+        String maxString = new Generics().findMaxString(name1,name2,name3);
+        System.out.println("Max String value is : " +maxString);
     }
 
     private Integer findMaxIntNum(Integer number1, Integer number2, Integer number3) {
@@ -31,5 +35,15 @@ public class Generics {
         }
         return maximum;
 
+    }
+    private String findMaxString(String name1, String name2, String name3) {
+        String max = name1;
+        if (name2.compareTo(max) > 0) {
+            max = name2;
+        }
+        if (name3.compareTo(max) > 0) {
+            max = name3;
+        }
+        return max;
     }
 }
