c5110bc8cf7c391234a329be234521e255db562a
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-03-08 20:59:08 +0530

    Refactor : code using generic class and static method

--- Generics.java
+++ Generics.java
@@ -1,22 +1,27 @@
 package com.assigenement.generics;
 
-public class Generics {
+public class Generics<T extends Comparable> {
+    T data1, data2, data3;
+
+    Generics(T data1, T data2, T data3) {
+        this.data1 = data1;
+        this.data2 = data2;
+        this.data3 = data3;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Generics Program ");
-        Generics findMaxIntNumber = new Generics();
         Integer number1 = 10, number2 = 10, number3 = 40;
         Float floatNumber1 = 2.5f, floatNumber2 = 10.3f, floatNumber3 = 7.6f;
         String name1 = "Apple", name2 = "Banana", name3 = "Mango";
+        System.out.println("Maximum Integer value is " + testMaximum(number1, number2, number3));
+        System.out.println("Maximum Float value is " + testMaximum(floatNumber1, floatNumber2, floatNumber3));
+        System.out.println("Maximum String value is " + testMaximum(name1, name2, name3));
 
-        Integer max = findMaxIntNumber.findMaxNum(number1, number2, number3);
-        System.out.println("Max  Integer number  is : " + max);
-        Float maxFloatNumber = new Generics().findMaxNum(floatNumber1, floatNumber2, floatNumber3);
-        System.out.println("Max float number is : " + maxFloatNumber);
-        String maxString = new Generics().findMaxNum(name1, name2, name3);
-        System.out.println("Max String value is : " + maxString);
+
     }
 
-    private <E extends Comparable> E findMaxNum(E name1, E name2, E name3) {
+    private static <E extends Comparable> E testMaximum(E name1, E name2, E name3) {
         E max = name1;
         if (name2.compareTo(max) > 0) {
             max = name2;
