a1e96977ddf00e62832d0ba53f86e9ce0a7bf04e
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-03-08 15:09:54 +0530

    Added : code for find max Int number

--- Generics.java
+++ Generics.java
@@ -3,5 +3,20 @@ package com.assigenement.generics;
 public class Generics {
     public static void main(String[] args) {
         System.out.println("Welcome to Generics Program ");
+        Generics findMaxIntNumber = new Generics();
+        Integer number1 = 10, number2 = 10, number3 = 40;
+        Integer max = findMaxIntNumber.findMaxIntNum(number1, number2, number3);
+        System.out.println("Max  Integer number  is : " + max);
+    }
+
+    private Integer findMaxIntNum(Integer number1, Integer number2, Integer number3) {
+        Integer maximum = number1;
+        if (number2.compareTo(maximum) > 0) {
+            maximum = number2;
+        }
+        if (number3.compareTo(maximum) > 0) {
+            maximum = number3;
+        }
+        return maximum;
     }
 }
