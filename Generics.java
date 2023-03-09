ebfea3b6a9ac03c935130ed89620f1c4676e787d
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-03-09 14:35:54 +0530

    Extend the max method to take more then three parameters

--- Generics.java
+++ Generics.java
@@ -1,35 +1,24 @@
 package com.assigenement.generics;
 
-public class Generics<T extends Comparable> {
-    T data1, data2, data3;
+public class Generics<T extends Comparable<T>> {
 
-    Generics(T data1, T data2, T data3) {
-        this.data1 = data1;
-        this.data2 = data2;
-        this.data3 = data3;
-    }
 
     public static void main(String[] args) {
+        Generics generics = new Generics();
         System.out.println("Welcome to Generics Program ");
-        Integer number1 = 10, number2 = 10, number3 = 40;
-        Float floatNumber1 = 2.5f, floatNumber2 = 10.3f, floatNumber3 = 7.6f;
-        String name1 = "Apple", name2 = "Banana", name3 = "Mango";
-        System.out.println("Maximum Integer value is " + testMaximum(number1, number2, number3));
-        System.out.println("Maximum Float value is " + testMaximum(floatNumber1, floatNumber2, floatNumber3));
-        System.out.println("Maximum String value is " + testMaximum(name1, name2, name3));
-
+        generics.testMaximum(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+        generics.testMaximum(1.1, 2.2, 3.3, 4.4, 5.5);
+        generics.testMaximum("Hello", "This", "Is", "String", "Array");
 
     }
 
-    private static <E extends Comparable> E testMaximum(E name1, E name2, E name3) {
-        E max = name1;
-        if (name2.compareTo(max) > 0) {
-            max = name2;
+    public T testMaximum(T... input) {
+        T max = input[0];
+        for (int i = 0; i < input.length; i++) {
+            if (input[i].compareTo(max) > 0)
+                max = input[i];
         }
-        if (name3.compareTo(max) > 0) {
-            max = name3;
-        }
+        System.out.println("The maximum Value is : " + max);
         return max;
     }
-
 }
